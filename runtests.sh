7774cfd100b5643197d04f1b88686b406e43c130
lmgv66 <51816696+lmgv66@users.noreply.github.com>
2023-05-26 08:27:13 +0200

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,3 @@
+echo "Executing Tests script" >> runtests.out
 RET=0
-
-echo "Testing from MAIN " >> runtests.out
-
-foo=$MY_SECRET
-ss=""
-for (( i=0; i<${#foo}; i++ )); do
-  #echo "${foo:$i:1}" >> runtests.out
-  ss="$ss$IFS${foo:$i:1}"
-done
-echo $ss >> runtests.out
-echo "Bye from MAIN [$RET]" >> runtests.out
-
 exit $RET
